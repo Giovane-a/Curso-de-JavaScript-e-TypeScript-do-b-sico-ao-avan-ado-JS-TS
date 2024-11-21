c81f34beadc7e75d10cf76cd7b0924a09e014164
Giovane Antonio de paiva <giovane-a.p@hotmail.com>
2024-11-20 22:46:39 -0300

    Create index.js

---
+++ aula8/index.js
@@ -0,0 +1,10 @@
+const nome = 'Luiz Otávio';
+const sobrenome= 'Miranda';
+const idade = 30;
+const peso = 84;
+const altura= 1.80;
+let imc;
+let anoNascimento = 1994;
+ imc =peso / (altura*altura)
+
+ console.log(nome, sobrenome, 'tem' ,idade, 'anos, pesa',peso, 'kg tem' ,altura, 'de altura e seu IMC é de',imc, nome, 'nasceu em' ,anoNascimento,) 
